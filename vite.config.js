7a849a6fc685c8b470682f07668ed4a7efffe127
Athreya <akondiathreya@gmail.com>
2026-05-02 15:44:33 +0530

    Change base path for GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/athreya-portfolio/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/gpp-athreya-portfolio/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
